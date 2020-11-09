92a5bd579f9f09370ea5e63fd6c1613061662485
Douglas de León <dele18037@uvg.edu.gt>
2020-11-08 19:16:08 -0600

    data para árbol

--- db.pl
+++ db.pl
@@ -3,11 +3,21 @@ loves(romeo, juliet).
 loves(juliet, romeo) :-
     loves(romeo, juliet).
 
+relacionado(X, Y) :-
+    progenitor(X, Z),
+    relacionado(Z, Y).
+
 
 progenitor('Douglas', 'Douglas Jr.').
 progenitor('Glendy', 'Douglas Jr.').
 progenitor('Douglas', 'Paola').
 progenitor('Glendy', 'Paola').
+progenitor('Johnny', 'Johnny Jr.').
+progenitor('María Elena', 'Johnny Jr.').
+progenitor('Johnny', 'Fernando').
+progenitor('María Elena', 'Fernando').
+progenitor('Johnny', 'María Alejandra').
+progenitor('María Elena', 'María Alejandra').
 
 mujer('Glendy').
 mujer('Paola').
@@ -37,21 +47,68 @@ madre('María Elena', 'María Alejandra').
 
 hijo('Douglas Jr.', 'Glendy').
 hijo('Douglas Jr.', 'Douglas').
+hijo('Johnny Jr.', 'Johnny').
+hijo('Johnny Jr.', 'Marían Elena').
+hijo('Fernando', 'Johnny').
+hijo('Fernando', 'Marían Elena').
 
 hija('Paola', 'Glendy').
+hija('Paola', 'Douglas').
+hija('María Andreé', 'Glendy').
+hija('María Andreé', 'Douglas').
+hija('María Alejandra', 'Johnny').
+hija('María Alejandra', 'Marían Elena').
 
-abuelo('René', 'Douglas').
+abuelo('René', 'Douglas Jr.').
+abuelo('René', 'María Andreé').
+abuelo('René', 'Paola').
+abuelo('René', 'Johnny Jr.').
+abuelo('René', 'Fernando').
+abuelo('René', 'María Alejandra').
 
-abuela('Margoth', 'Douglas').
+abuelo('Margoth', 'Douglas Jr.').
+abuelo('Margoth', 'María Andreé').
+abuelo('Margoth', 'Paola').
+abuelo('Margoth', 'Johnny Jr.').
+abuelo('Margoth', 'Fernando').
+abuelo('Margoth', 'María Alejandra').
 
-hermano('Douglas', 'Paola').
+hermano('Douglas Jr.', 'Paola').
+hermano('Douglas Jr.', 'María Andreé').
+hermano('Fernando', 'María Alejandra').
+hermano('Johnny Jr.', 'María Alejandra').
 
-hermana('Paola', 'Douglas').
+hermana('Paola', 'María Andreé').
+hermana('Paola', 'Douglas Jr.').
+hermana('María Andreé', 'Paola').
+hermana('María Andreé', 'Douglas Jr.').
+hermana('María Alejandra', 'Fernando').
+hermana('María Alejandra', 'Johnny Jr.').
 
-tio('Johnny', 'Douglas').
+tio('Johnny', 'Douglas Jr.').
+tio('Johnny', 'Paola').
+tio('Johnny', 'María Andreé').
+tio('Douglas', 'Johnny Jr.').
+tio('Douglas', 'Fernando').
+tio('Douglas', 'María Alejandra').
 
-tia('María Elena', 'Douglas').
+tia('María Elena', 'Douglas Jr.').
+tia('María Elena', 'Paola').
+tia('María Elena', 'María Andreé').
+tia('Glendy', 'Johnny Jr.').
+tia('Glendy', 'Fernando').
+tia('Glendy', 'María Alejandra').
 
-sobrino('Douglas', 'Johnny').
+sobrino('Douglas Jr.', 'Johnny').
+sobrino('Douglas Jr.', 'María Elena').
+sobrino('Fernando', 'Douglas').
+sobrino('Johnny jr.', 'Douglas').
+sobrino('Fernando', 'Glendy').
+sobrino('Johnny jr.', 'Glendy').
 
-sobrina('Paola', 'Johnny').
+sobrina('Paola Jr.', 'Johnny').
+sobrina('Paola Jr.', 'María Elena').
+sobrina('María Andreé', 'Johnny').
+sobrina('María Andreé', 'María Elena').
+sobrina('María Alejandra', 'Douglas').
+sobrina('María Alejandra', 'Glendy').
