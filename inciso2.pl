932c159fa0ea8e6879a5850e6b51d4b9886eab51
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2020-11-13 12:15:39 -0600

    Format update

--- inciso2.pl
+++ inciso2.pl
@@ -1,33 +1,28 @@
 /* Inciso 2 Proyecto 4 LM
 Caminos entre nodos
 Referencia: https://www.cpp.edu/~jrfisher/www/prolog_tutorial/2_15pl.txt
-
-camino(X, Y, [X|Cs]) :-
-	camino(X, Y, [X], Cs).
-
-
-camino(X, Y, Visitados, [Z|Cs]) :-
-	edge(X, Z),
-	\+member(Z, Visitados),
-	camino(Z, Y, [Z|Visitados], Cs).
-camino(X, X, _, []).
-
 */
 
 camino(A,B,Path) :-
 	travel(A,B,[A],Q),
-	reverse(Q, Path).
-
+	rev(Q, Path).
 
 
 travel(A,B,Visited, Path) :-
        edge(A,C),           
        C \== B,
-       \+member(C,Visited),
+       member(C,Visited) \== false,
        travel(C,B,[C|Visited],Path).
+
 
 travel(A, B, P, [B|P]) :-
 	edge(A,B).
+
+
+rev(L,R):-  accRev(L,[],R).
+accRev([H|T],A,R):-  accRev(T,[H|A],R).
+accRev([],A,A).
+
 
 % Digraph
 edge(a, b).
